4ad77065f6305f64ae47baa7eb5891fc3c4ce2d9
Matheus Meigre <matheusmeigre@hotmail.com>
2025-04-25 16:43:00 -0300

    create DBSet Contacts

--- ContactsControl/Data/BancoContext.cs
+++ ContactsControl/Data/BancoContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ContactsControl.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace ContactsControl.Data
         { 
         
         }
+
+        public DbSet<ContactModel> Contacts { get; set; }
     }
 }
