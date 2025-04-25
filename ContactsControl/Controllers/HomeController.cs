7b6ea4e6929a0bc7253d1c22c204d20b1413fb19
Matheus Meigre <matheusmeigre@hotmail.com>
2025-04-25 12:03:44 -0300

    start my web application

---
+++ ContactsControl/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using ContactsControl.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsControl.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
